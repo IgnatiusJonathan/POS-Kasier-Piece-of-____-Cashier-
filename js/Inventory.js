c65a491ee9b89e04071df83741080f087679c415
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-09-30 19:27:45 +0700

    Update Inventory.js

--- js/Inventory.js
+++ js/Inventory.js
@@ -1,1 +1,5 @@
 let inventory = document.getElementById("inventory")
+let products = JSON.parse(localStorage.getItem("ProductID")) || [];
+function renderInventory(){
+  if(products.length ===0) inventory.innerHTML= "<p>Inventory kosong<p>"
+}
