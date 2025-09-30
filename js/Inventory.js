b60cce21aa5f71d8317713b37b51db5f6915ca0f
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-09-30 19:46:30 +0700

    Update Inventory.js

--- js/Inventory.js
+++ js/Inventory.js
@@ -1,15 +1,20 @@
 let inventory = document.getElementById("inventory")
 let products = JSON.parse(localStorage.getItem("ProductID")) || [];
+
 function renderInventory(){
+  inventory.innerHTML = "";
+  
   if(products.length ===0) inventory.innerHTML= `<p>Inventory kosong<p>`
   else{
-    let productPreview = document.createElement("productPreview");
+    let productPreview = document.createElement("div");
+    productPreview.className = "productPreview";
+    
     products.forEach(p =>{
-      let pro = document.createElement("pro");
+      let pro = document.createElement("div");
       pro.classname = "inventorySlot"
       pro.innerHTML = `
       <span class="name">${p.name}</span>
-      <span class="image">${p.image}</span>
+      <img class="image" src="${p.image}" alt="${p.name}">
       <span class="ID">${p.productID}</span>
       <span class="price">${p.price}</span>
       <span class="amount">${p.amount}</span>
@@ -18,3 +23,5 @@ function renderInventory(){
     });
     inventory.appendChild(productPreview);
 }
+
+renderInventory();
