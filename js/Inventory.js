f5218de8ea1746215b996fef3bb0f986bd647519
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-10-01 21:50:30 +0700

    Update Inventory.js

--- js/Inventory.js
+++ js/Inventory.js
@@ -6,12 +6,9 @@ function renderInventory(){
   
   if(products.length ===0) inventory.innerHTML= `<p>Inventory kosong<p>`
   else{
-    let productPreview = document.createElement("div");
-    productPreview.className = "productPreview";
-    
     products.forEach(p =>{
       let pro = document.createElement("div");
-      pro.classname = "inventorySlot"
+      pro.className = "inventorySlot"
       pro.innerHTML = `
       <span class="name">${p.name}</span>
       <img class="image" src="${p.image}" alt="${p.name}">
@@ -19,9 +16,8 @@ function renderInventory(){
       <span class="price">${p.price}</span>
       <span class="amount">${p.amount}</span>
       `;
-      productPreview.appendChild(pro);
+      inventory.appendChild(pro);
     });
-    inventory.appendChild(productPreview);
 }
 
 renderInventory();
