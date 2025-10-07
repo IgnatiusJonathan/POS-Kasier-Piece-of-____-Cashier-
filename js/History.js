5b50d6076eba4c2dc61aaedda9f4fd07c1f648cc
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-10-07 19:46:53 +0700

    Update History.js
    Tambah fitur filter

--- js/History.js
+++ js/History.js
@@ -1,35 +1,63 @@
 let history = JSON.parse(sessionStorage.getItem("History")) || [];
-    let historyList = document.getElementById("history");
+let historyList = document.getElementById("history");
+let filterDate = document.getElementById("filterDate");
+const filterBtn = document.getElementById("filterBtn");
+const resetBtn = document.getElementById("resetBtn");
 
-    if (history.length === 0) {
-            historyList.innerHTML = "<p>No purchases yet.</p>";
-    } else {
-        let ul = document.createElement("ul");
+function renderHistory(list = history) {
+    historyList.innerHTML = "";
 
-        let headerLi = document.createElement("li");
-        headerLi.className = "head";
-        headerLi.innerHTML = `
-            <span class="item">Product</span>
-            <span class="amount">Jumlah</span>
-            <span class="price">Harga</span>
-            <span class="date">Tanggal Pembelian</span>
-            <span class="costumer">Pembeli</span>
-            <span class="cashier">Kasier</span>
+    if (list.length === 0) {
+        historyList.innerHTML = "<p>No purchases yet.</p>";
+        return;
+      }
+  
+      let ul = document.createElement("ul");
+  
+      let headerLi = document.createElement("li");
+      headerLi.className = "head";
+      headerLi.innerHTML = `
+        <span class="item">Product</span>
+        <span class="amount">Jumlah</span>
+        <span class="price">Harga</span>
+        <span class="date">Tanggal Pembelian</span>
+        <span class="costumer">Pembeli</span>
+        <span class="cashier">Kasier</span>
+      `;
+      ul.appendChild(headerLi);
+  
+      list.forEach(h => {
+        let li = document.createElement("li");
+        li.className = "historyBox";
+        li.innerHTML = `
+          <span class="item">${h.item}</span>
+          <span class="amount">${h.amount}</span>
+          <span class="price">Rp ${h.price}</span>
+          <span class="date">${h.date}</span>
+          <span class="costumer">${h.costumer}</span>
+          <span class="cashier">${h.cashier}</span>
         `;
+        ul.appendChild(li);
+      });
+  
+      historyList.appendChild(ul);
+    }
 
-        ul.appendChild(headerLi);
-            history.forEach(h => {
-                let li = document.createElement("li");
-                li.className = "historyBox";
-                li.innerHTML = `
-                    <span class="item">${h.item}</span>
-                    <span class="amount">${h.amount}</span>
-                    <span class="price">Rp ${h.price}</span>
-                    <span class="date">${h.date}</span>
-                    <span class="costumer">${h.costumer}</span>
-                    <span class="cashier">${h.cashier}</span>
-                `;
-                ul.appendChild(li);
-            });
-            historyList.appendChild(ul);
-        }
+function filterHistory() {
+    const selectedDate = filterDate.value;
+    if (!selectedDate) {
+        renderHistory(history);
+        return;
+    }
+      
+    const filtered = history.filter(h => h.date === selectedDate);
+    renderHistory(filtered);
+}
+
+filterBtn.addEventListener("click", filterHistory);
+  resetBtn.addEventListener("click", () => {
+    filterDate.value = "";
+    renderHistory(history);
+});
+
+renderHistory();
